9c4889c9922c5b8e91a27fc35614bc4d39bcac39
Juliano Santos <julianosantosdev@outlook.com>
2023-03-13 16:31:58 -0300

    add: modal habbits api requisition and data treatment

--- src/components/Modal/ExerciseHours/index.tsx
+++ src/components/Modal/ExerciseHours/index.tsx
@@ -4,16 +4,45 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { IHabbitsForm } from "../Template/type";
 import { modalFormSchema } from "../Template/formSchema";
+import { toast } from "react-toastify";
+import { api } from "../../../services/api";
+import { useContext } from "react";
+import { UserContext } from "../../../contexts/UserContext";
 
 const ExerciseHours = () => {
+  const { training, setTraining } = useContext(UserContext);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<IHabbitsForm>({ resolver: yupResolver(modalFormSchema) });
 
-  const submitHabbit: SubmitHandler<IHabbitsForm> = (data) => {
-    console.log(data);
+  const userId = Number(localStorage.getItem("@ID-HABBITS"));
+  const token = localStorage.getItem("@TOKEN-HABBITS");
+
+  const submitHabbit: SubmitHandler<IHabbitsForm> = async (formData) => {
+    const { date, hours } = formData;
+
+    const formDataToSubmit = {
+      date: date,
+      hours: parseInt(hours),
+      userId: userId,
+    };
+    console.log(formDataToSubmit);
+
+    try {
+      const response = await api.post(`/trainingTimes`, formDataToSubmit, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      toast.success("Hábito registrado!");
+      console.log(response.data);
+      // setTraining()
+    } catch (error) {
+      toast.error("Erro ao registrar hábito");
+      console.log(error);
+    }
   };
 
   return (
--- src/components/Modal/StudyHours/index.tsx
+++ src/components/Modal/StudyHours/index.tsx
@@ -1,19 +1,48 @@
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useContext } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { toast } from "react-toastify";
+import { UserContext } from "../../../contexts/UserContext";
+import { api } from "../../../services/api";
 import { StyledButton } from "../../../styles/ButtonStyles";
 import { Input } from "../../Input";
 import { modalFormSchema } from "../Template/formSchema";
 import { IHabbitsForm } from "../Template/type";
 
 const StudyHours = () => {
+  const { study, setStudy } = useContext(UserContext);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<IHabbitsForm>({ resolver: yupResolver(modalFormSchema) });
 
-  const submitHabbit: SubmitHandler<IHabbitsForm> = (data) => {
-    console.log(data);
+  const userId = Number(localStorage.getItem("@ID-HABBITS"));
+  const token = localStorage.getItem("@TOKEN-HABBITS");
+
+  const submitHabbit: SubmitHandler<IHabbitsForm> = async (formData) => {
+    const { date, hours } = formData;
+
+    const formDataToSubmit = {
+      date: date,
+      hours: parseInt(hours),
+      userId: userId,
+    };
+    console.log(formDataToSubmit);
+
+    try {
+      const response = await api.post(`/studyTimes`, formDataToSubmit, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      toast.success("Hábito registrado!");
+      console.log(response.data);
+      // setSleep()
+    } catch (error) {
+      toast.error("Erro ao registrar hábito");
+      console.log(error);
+    }
   };
 
   return (
