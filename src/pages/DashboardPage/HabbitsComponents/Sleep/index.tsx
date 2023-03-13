b422b6183bd8fece848c34493dabf6681f52db22
Juliano Santos <julianosantosdev@outlook.com>
2023-03-13 12:39:50 -0300

    add: radial chart to dashboard

--- src/pages/DashboardPage/HabbitsComponents/Sleep/index.tsx
+++ src/pages/DashboardPage/HabbitsComponents/Sleep/index.tsx
@@ -24,10 +24,43 @@ const SleepHabbits = () => {
     ],
   };
 
+  const doughnutData = sleep.reduce((acc, cur) => acc + cur.hours, 0);
+  const value = doughnutData / userData.length;
+  const datatochart = [
+    {
+      label: "media",
+      value: value.toFixed(1),
+    },
+    {
+      label: "Recomendadas",
+      value: (8 - value).toFixed(1),
+    },
+  ];
+
+  const teste2 = {
+    labels: datatochart.map((item) => item.label),
+    datasets: [
+      {
+        label: "",
+        data: datatochart.map((item) => item.value),
+        backgroundColor: ["#AA66FF", "#ffffff"],
+        borderWidth: 2,
+        borderRadius: 5,
+        borderSkipped: false,
+        cutout: "75%",
+        radius: "75%"
+      },
+    ],
+  };
+
+
   return (
     <div className="habbitInfo">
       <h4>Sono</h4>
+      <button>adicionar</button>
       <BarChart chartData={teste} />
+      <DoughnutChart chartData={teste2} />
+      <p>{value.toFixed(1)}</p>
     </div>
   );
 };
