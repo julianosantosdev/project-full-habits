c171316b0770080f4f3c53c8f5bd91f50d120408
Juliano Santos <julianosantosdev@outlook.com>
2023-03-13 22:08:12 -0300

    refactor: regex on modal form

--- src/components/Modal/Template/formSchema.ts
+++ src/components/Modal/Template/formSchema.ts
@@ -2,8 +2,21 @@ import * as yup from "yup";
 
 export const modalFormSchema = yup
   .object({
-    date: yup.string().required("A data é obrigatória"),
-    hours: yup.string().required("Quantidade de horas é obrigatória"),
+    date: yup
+      .string()
+      .required("A data é obrigatória")
+      .matches(
+        /^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}$/,
+        "O formato deve ser DD/MM/AAAA"
+      ),
+    hours: yup
+      .number()
+      .required("Quantidade de horas é obrigatória")
+      .test(
+        "Is positive?",
+        "Valor deve ser entre 0 e 24",
+        (value) => value >= 0 && value <= 24
+      ),
   })
   .required();
 
